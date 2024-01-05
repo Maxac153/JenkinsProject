855446974e0f9d5fff4cb537e06f199ce5942858
turgor <maxac153@gmail.com>
2024-01-05 22:13:28 +0500

    add tests to project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 
+
 if __name__ == '__main__':
     with open('file.txt', 'w') as f:
         f.write('Hello Jenkins.\n')
---
+++ tests/fo_test.py
@@ -0,0 +1,7 @@
+def fo(number_one: int, number_two: int):
+    return number_one + number_two
+
+
+def test_fo():
+    assert fo(10, 5) == 15.0
+    # assert fo(10, 5) == 10.0
